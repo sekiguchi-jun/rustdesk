a3069e2e9065b4ac3e0817a8e1675e2501939491
sekiguchi-jun <sekiguchijun@fgate-inc.com>
2026-03-27 13:35:09 +0900

    fix(macos): detect app bundle name dynamically for FGATE Support build

--- build.py
+++ build.py
@@ -411,7 +411,10 @@ def build_flutter_dmg(version, features):
         "cp target/release/liblibrustdesk.dylib target/release/librustdesk.dylib")
     os.chdir('flutter')
     system2('flutter build macos --release')
-    system2('cp -rf ../target/release/service ./build/macos/Build/Products/Release/RustDesk.app/Contents/MacOS/')
+    import glob as _glob
+    _apps = _glob.glob('./build/macos/Build/Products/Release/*.app')
+    _app = _apps[0] if _apps else './build/macos/Build/Products/Release/RustDesk.app'
+    system2('cp -rf ../target/release/service "%s/Contents/MacOS/"' % _app)
     '''
     system2(
         "create-dmg --volname \"RustDesk Installer\" --window-pos 200 120 --window-size 800 400 --icon-size 100 --app-drop-link 600 185 --icon RustDesk.app 200 190 --hide-extension RustDesk.app rustdesk.dmg ./build/macos/Build/Products/Release/RustDesk.app")
